d8087dfc335789a2db64b8a95be4fb029f935871
Parth Shah <shahp@mit.edu>
2019-02-28 20:17:54 -0500

    Add contains to esqueleto postgis

--- src/Database/Esqueleto/Postgis/Geography.hs
+++ src/Database/Esqueleto/Postgis/Geography.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE MultiParamTypeClasses, OverloadedStrings #-}
 
-module Database.Esqueleto.Postgis.Geography (intersects) where
+module Database.Esqueleto.Postgis.Geography (contains, intersects) where
 
 import Database.Esqueleto (Esqueleto, SqlBackend, SqlExpr, SqlQuery, Value)
 import Database.Esqueleto.Internal.Sql (unsafeSqlFunction)
@@ -11,9 +11,13 @@ instance IsGeography (Geography a)
 instance IsGeography a => IsGeography (Maybe a)
 
 class Esqueleto query expr backend => EsqueletoGeography query expr backend where
+    contains :: (IsGeography a, IsGeography b)
+             => expr (Value a) -> expr (Value b) -> expr (Value Bool)
     intersects :: (IsGeography a, IsGeography b)
                => expr (Value a) -> expr (Value b) -> expr (Value Bool)
+infixl 7 `contains`
 infixl 7 `intersects`
 
 instance EsqueletoGeography SqlQuery SqlExpr SqlBackend where
+    contains a b = unsafeSqlFunction "ST_Contains" (a, b)
     intersects a b = unsafeSqlFunction "ST_Intersects" (a, b)

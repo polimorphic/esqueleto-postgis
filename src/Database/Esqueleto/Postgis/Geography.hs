f7a0f67f1cf8c237b0d7578101b5a99c5abe64dc
Parth Shah <shahp@mit.edu>
2019-03-09 02:28:04 -0500

    Updated to drop contains which is broken for geo and updated persistent postgis dependency

--- src/Database/Esqueleto/Postgis/Geography.hs
+++ src/Database/Esqueleto/Postgis/Geography.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, OverloadedStrings #-}
 
-module Database.Esqueleto.Postgis.Geography (contains, intersects) where
+module Database.Esqueleto.Postgis.Geography (intersects) where
 
 import Database.Esqueleto (Esqueleto, SqlBackend, SqlExpr, SqlQuery, Value)
 import Database.Esqueleto.Internal.Sql (unsafeSqlFunction)
@@ -14,13 +14,9 @@ instance IsGeography (Some Geography)
 instance IsGeography a => IsGeography (Maybe a)
 
 class Esqueleto query expr backend => EsqueletoGeography query expr backend where
-    contains :: (IsGeography a, IsGeography b)
-             => expr (Value a) -> expr (Value b) -> expr (Value Bool)
     intersects :: (IsGeography a, IsGeography b)
                => expr (Value a) -> expr (Value b) -> expr (Value Bool)
-infixl 7 `contains`
 infixl 7 `intersects`
 
 instance EsqueletoGeography SqlQuery SqlExpr SqlBackend where
-    contains a b = unsafeSqlFunction "ST_Contains" (a, b)
     intersects a b = unsafeSqlFunction "ST_Intersects" (a, b)

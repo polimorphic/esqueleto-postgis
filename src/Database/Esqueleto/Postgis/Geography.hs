c3dfd7cb0e69b935d06a583e8aefa943c7546c36
tysonzero <danielspencersmith@gmail.com>
2020-03-21 04:59:40 -0400

    Drop usage of removed Esqueleto class

--- src/Database/Esqueleto/Postgis/Geography.hs
+++ src/Database/Esqueleto/Postgis/Geography.hs
@@ -2,21 +2,18 @@
 
 module Database.Esqueleto.Postgis.Geography (intersects) where
 
-import Database.Esqueleto (Esqueleto, SqlBackend, SqlExpr, SqlQuery, Value)
+import Database.Esqueleto (SqlExpr, Value)
 import Database.Esqueleto.Internal.Sql (unsafeSqlFunction)
 import Data.Geometry.Geos.Geometry (Some)
 import Database.Persist.Postgis.Geography (Geography)
-
 
 class IsGeography g
 instance IsGeography (Geography a)
 instance IsGeography (Some Geography)
 instance IsGeography a => IsGeography (Maybe a)
 
-class Esqueleto query expr backend => EsqueletoGeography query expr backend where
-    intersects :: (IsGeography a, IsGeography b)
-               => expr (Value a) -> expr (Value b) -> expr (Value Bool)
+intersects :: (IsGeography a, IsGeography b)
+           => SqlExpr (Value a) -> SqlExpr (Value b) -> SqlExpr (Value Bool)
+intersects a b = unsafeSqlFunction "ST_Intersects" (a, b)
 infixl 7 `intersects`
 
-instance EsqueletoGeography SqlQuery SqlExpr SqlBackend where
-    intersects a b = unsafeSqlFunction "ST_Intersects" (a, b)
